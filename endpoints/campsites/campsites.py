2513852377794b7e3518ab2531a467b26b1ad2fb
russelry <46463469+russelry@users.noreply.github.com>
2022-04-18 20:40:31 -0700

    Create campsites.py

---
+++ endpoints/campsites/campsites.py
@@ -0,0 +1,71 @@
+import os
+import json
+import pymysql
+
+# https://stackify.com/aws-lambda-with-python-a-complete-getting-started-guide/
+# https://docs.aws.amazon.com/lambda/latest/dg/python-image.html
+# https://pypi.org/project/PyMySQL/#documentation
+
+
+MYSQL_HOST = os.environ['MYSQL_HOST']
+MYSQL_PORT = os.environ['MYSQL_PORT']
+MYSQL_DATABASE = os.environ['MYSQL_DATABASE']
+MYSQL_USER = os.environ['MYSQL_USER']
+MYSQL_PASS = os.environ['MYSQL_PASS']
+
+
+def query_mysql() -> dict:
+    result: dict = None
+    destination_table: str = "Campsites"
+    
+    conn = pymysql.connect(
+        host=MYSQL_HOST,
+        user=MYSQL_USER,
+        port=int(MYSQL_PORT),
+        passwd=MYSQL_PASS,
+        db=MYSQL_DATABASE,
+        connect_timeout=5
+    )
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(f"SELECT * FROM {destination_table} LIMIT 10")
+            result = cursor.fetchall()
+
+    except Exception as e:
+        print(e)
+
+    finally:
+        if(conn is not None and conn.open):
+            conn.close()
+
+    return result
+
+
+def transform(records: list) -> list:
+    transform_data: list = []
+    for record in records:
+        data: dict = {}
+        data['campsite_id'] = record[0]
+        data['facility_id'] = record[1]
+        data['campsite_name'] = record[2]
+        data['campsite_type'] = record[3] 
+        data['type_of_use'] = record[4] 
+        data['campsite_accessible'] = record[5] 
+        data['campsite_reservable'] = record[6] 
+        data['campsite_longitude'] = record[7] 
+        data['campsite_latitude'] = record[8] 
+        transform_data.append(data)
+    return transform_data
+
+
+def handler(event, context):
+    query_result: dict = query_mysql()   
+    transformed_result: list = transform(records=query_result)
+ 
+    return {
+        'statusCode': 200,
+        'body': transformed_result,
+        "headers": {
+            'Content-Type': 'application/json'
+        }
+    }
